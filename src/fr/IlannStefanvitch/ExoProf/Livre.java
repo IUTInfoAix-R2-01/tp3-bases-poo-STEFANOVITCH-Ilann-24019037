38f7e2f625e4570932a03f38c52fe52d31651b31
STEFANOVITCH-Ilann-24019037 <ilann.stefanovitch@etu.univ-amu.fr>
2025-02-04 13:55:08 +0100

    Tout l'exo

--- src/fr/IlannStefanvitch/ExoProf/Livre.java
+++ src/fr/IlannStefanvitch/ExoProf/Livre.java
@@ -1,5 +1,45 @@
 package fr.IlannStefanvitch.ExoProf;
 
 public class Livre {
+	
+	private String name;
+	private float price;
+	private int quatite;
+	
+	public Livre () {
+		name = "Jean";
+		price = 10 ;
+		quatite = 50;
+	}
+	public Livre (String name , float price , int quatite) {
+		this.name = name;
+		this.price = price ;
+		this.quatite = quatite;
+	}
+	
 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getQuatite() {
+		return quatite;
+	}
+	public void setQuatite(int quatite) {
+		this.quatite = quatite;
+	}
+	
+	public String toString() {
+		return " Titre : " + name + " Pirx :" + price + " Qtt " + quatite;
+	}
+	
+	
 }
